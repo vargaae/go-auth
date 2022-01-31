82051caecc397afa504c605bd857010e5796f202
Andras Varga <vargaae@hotmail.com>
2022-01-31 23:44:49 +0100

    connect to mysql with GORM

--- Main.go
+++ Main.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+	_, err := gorm.Open(mysql.Open("root:rootroot@/smart_brain"), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
